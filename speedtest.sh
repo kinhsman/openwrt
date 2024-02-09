c233998a242627125099ae8b06dbc778a950ba57
kinhsman <145226962+kinhsman@users.noreply.github.com>
2024-02-09 12:43:59 -0600

    Update speedtest.sh
    add alias

--- speedtest.sh
+++ speedtest.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-
 # Download the Speedtest CLI
 wget -O speedtest.tar.gz https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-x86_64.tgz
 
 # Extract the downloaded archive
 tar zxvf speedtest.tar.gz
 
-# Run the Speedtest CLI
-./speedtest
+# Check if .ashrc or .profile exists, if not, create it
+if [ ! -f ~/.ashrc ]; then
+    touch ~/.ashrc
+fi
+
+if [ ! -f ~/.profile ]; then
+    touch ~/.profile
+fi
+
+# Add an alias for speedtest
+echo "alias speedtest=\"$(pwd)/speedtest\"" >> ~/.ashrc
+echo "alias speedtest=\"$(pwd)/speedtest\"" >> ~/.profile
+
+# Source the .ashrc or .profile file to apply the changes immediately
+source ~/.ashrc || source ~/.profile
+
+echo "Speedtest CLI has been installed. You can now use 'speedtest' command."
