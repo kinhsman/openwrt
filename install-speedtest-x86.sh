2837055135b0863e2db30176e1b9f7bf71772827
kinhsman <145226962+kinhsman@users.noreply.github.com>
2024-02-09 13:45:08 -0600

    Create install-speedtest-x86.sh

---
+++ install-speedtest-x86.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Download the Speedtest CLI
+wget -O speedtest.tar.gz https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-x86_64.tgz
+
+# Extract the downloaded archive
+tar zxvf speedtest.tar.gz
+
+# Check if .ashrc or .profile exists, if not, create it
+if [ ! -f ~/.ashrc ]; then
+    touch ~/.ashrc
+fi
+
+if [ ! -f ~/.profile ]; then
+    touch ~/.profile
+fi
+
+# Add an alias for speedtest
+echo "alias speedtest=\"$(pwd)/speedtest\"" >> ~/.ashrc
+echo "alias speedtest=\"$(pwd)/speedtest\"" >> ~/.profile
+
+# Source the .ashrc or .profile file to apply the changes immediately
+source ~/.ashrc || source ~/.profile
+
+#Completion
+echo -e "\e[32mSpeedtest CLI has been installed. You can now use 'speedtest' command.\e[0m"
+echo -e "\e[32mPlease log out and log back in to apply the changes.\e[0m"
